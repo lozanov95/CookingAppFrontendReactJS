8ef1b75eeee87e0888703ff4b9239a36b0e60913
lozanov95 <vasillozanov1@gmail.com>
2021-05-02 18:06:02 +0300

    added routing for register and home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './style/app.css';
 import { Navigation } from './views/navigation.js';
+import { CardList } from './views/card.js';
 import { Register } from './views/register.js';
-
+/*
 function App() {
   return (
     <div>
-      <Navigation />
-      <Register />
+    <Navigation />
+    <Register />
     </div>
-  );
+    );
+  }
+  */
+
+class App extends Component {
+  render() {
+    return <Router>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={CardList} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
+  }
 }
 
 export default App;
