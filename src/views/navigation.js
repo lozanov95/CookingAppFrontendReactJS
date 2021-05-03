b4feaa929f53305ed52989cc57a6f1caf1948c83
lozanov95 <vasillozanov1@gmail.com>
2021-05-03 22:47:11 +0300

    logout redirects to the home page

--- src/views/navigation.js
+++ src/views/navigation.js
@@ -24,6 +24,7 @@ export class Navigation extends Component {
     async handleClick(ev) {
         ev.preventDefault();
         await logout();
+        window.location = '/'
         this.checkState();
     }
 
