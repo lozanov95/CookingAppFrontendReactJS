c40c08d078941e6a3aed16fc1f2e4ee35b926a8d
lozanov95 <vasillozanov1@gmail.com>
2021-05-04 16:36:18 +0300

    added myrecipe view

---
+++ src/views/myrecipe.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { getRecipesForCurrentUser } from '../api/data.js';
+import { CardSection } from '../views/generic-components.js';
+
+export class MyRecipesView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { recipes: [], emptyMessage: 'Loading...' }
+    }
+
+    async componentDidMount() {
+        const recipes = await getRecipesForCurrentUser();
+        const emptyMessage = 'There are no recipes yet.'
+        this.setState({ recipes, emptyMessage })
+    }
+
+    render() {
+        return (
+            <CardSection recipes={this.state.recipes} emptyMessage={this.state.emptyMessage} />
+        )
+    }
+}
