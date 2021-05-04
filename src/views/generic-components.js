420d9867a5487c96f67fef8b07253a34a3fe9e26
lozanov95 <vasillozanov1@gmail.com>
2021-05-04 16:34:24 +0300

    added card and CardSection to the generic comps

--- src/views/generic-components.js
+++ src/views/generic-components.js
@@ -44,3 +44,27 @@ export function SubmitRecipe(props) {
         </form>
     );
 }
+
+export function Card(props) {
+    return (
+        <div className="card">
+            <div>
+                <h3>{props.name}</h3>
+                <img className="card-img" src={props.image_url} alt={props.name} />
+            </div>
+            <div>
+                <a className="anchor-btn" href={props.href_url}>View</a>
+            </div>
+        </div>
+    );
+}
+
+export function CardSection(props) {
+    return (
+        <div className="card-section">
+            {props.recipes.length > 0 ?
+                props.recipes.map((el) => <Card key={el.id} name={el.name} image_url={el.image_url} href_url={'/details/' + el.id} />) :
+                <div>{props.emptyMessage}</div>}
+        </div>
+    )
+}
